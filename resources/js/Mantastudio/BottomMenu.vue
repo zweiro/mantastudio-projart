7e4e5fa2f8c49a9c1b39748fbd82b6186ddaf50e
Robin Zweifel <robin.zweifel@gmail.com>
2021-06-08 15:29:32 +0200

    feat: add mantastudio structure

---
+++ resources/js/Mantastudio/BottomMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="w-full flex justify-evenly sm:justify-center fixed bottom-0 h-16 border-t-2 border-gray-500">
+      <bottom-menu-item>Test 1</bottom-menu-item>
+      <bottom-menu-item>Test 2</bottom-menu-item>
+      <bottom-menu-item>Test 3</bottom-menu-item>
+      <bottom-menu-item>Test 4</bottom-menu-item>
+  </ul>
+</template>
+
+<script>
+import BottomMenuItem from './BottomMenuItem'
+
+export default {
+    mounted() {
+        console.log("MantaStudio menu mounted!");
+    },
+    components: {
+        BottomMenuItem,
+    }
+}
+</script>
+
+<style>
+    
+</style>
---
+++ resources/js/Mantastudio/BottomMenuItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="h-full p-4">
+      <slot></slot>
+  </li>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    li {
+        color:tomato;
+    }
+</style>
