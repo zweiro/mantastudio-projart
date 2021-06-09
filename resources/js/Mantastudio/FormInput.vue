05d2333de0bb8fd7b893ae983ee22a21fc01a3d5
Nathan Zweifel <nathan.zweifel@heig-vd.ch>
2021-06-09 16:28:38 +0200

    style: add blue color for active inputs

--- resources/js/Mantastudio/FormInput.vue
+++ resources/js/Mantastudio/FormInput.vue
@@ -1,20 +1,24 @@
 <template>
-    <label :for="name" class="font-bold"><slot></slot></label>
-    <div class="flex items-center border-b border-black mb-10 mt-2">   
-        <input :name="name" class="appearance-none bg-transparent border-none w-full text-gray-700 p-0 leading-tight focus:outline-none" :type="type">
+    <label :for="name" class="font-bold">{{label}}</label>
+    <div class="focus-within:border-blue-400 flex items-center border-b border-black mb-10 mt-2">   
+        <input :name="name" class="form-input appearance-none bg-transparent border-none w-full text-gray-700 p-0 leading-tight" :type="type">
     </div>
 </template>
+
+// focus-within:border-blue-400
 
 <script>
 export default {
     props: {
         name: String,
-        type: String
+        type: String,
+        label: String
     },
     setup(props) {
         return {
             name: props.name,
-            type: props.type
+            type: props.type,
+            label: props.label
         }
     }
     
@@ -22,5 +26,10 @@ export default {
 </script>
 
 <style scoped>
+    .form-input:focus {
+            border-color: inherit;
+            -webkit-box-shadow: none;
+            box-shadow: none;
+        }
 
 </style>
