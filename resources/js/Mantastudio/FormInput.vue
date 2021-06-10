b7c5792886a41e1438dd64cde5c7080b2896cf37
Nathan Zweifel <nathan.zweifel@heig-vd.ch>
2021-06-10 11:21:34 +0200

    style: updated input and select

--- resources/js/Mantastudio/FormInput.vue
+++ resources/js/Mantastudio/FormInput.vue
@@ -1,11 +1,9 @@
 <template>
     <label :for="name" class="font-bold">{{label}}</label>
-    <div class="focus-within:border-blue-400 flex items-center border-b border-black mb-10 mt-2">   
+    <div class="focus-within:border-blue-400 flex items-center border-b-2 border-black mb-10 mt-2">   
         <input :name="name" class="form-input appearance-none bg-transparent border-none w-full text-gray-700 p-0 leading-tight" :type="type">
     </div>
 </template>
-
-// focus-within:border-blue-400
 
 <script>
 export default {
--- resources/js/Mantastudio/FormSelect.vue
+++ resources/js/Mantastudio/FormSelect.vue
@@ -1,7 +1,7 @@
 <template>
     <label :for="name" class="font-bold">{{label}}</label>
-    <div class="focus-within:border-blue-400 flex items-center border-b border-black mb-10 mt-2">   
-        <select :name="name" class="form-select w-auto appearance-none bg-transparent border-none w-full text-gray-700 p-0">
+    <div class="focus-within:border-blue-400 flex items-center border-b-2 border-black mb-10 mt-2">   
+        <select :name="name" class="form-select appearance-none bg-transparent border-none w-full text-gray-700 p-0">
             <slot></slot>
         </select>
     </div>
