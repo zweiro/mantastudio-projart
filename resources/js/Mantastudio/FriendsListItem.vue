71d6e1d8356d3f74a5169fa6bb6eeeb7afa55e64
Nathan Zweifel <nathan.zweifel@heig-vd.ch>
2021-06-10 16:28:41 +0200

    feat: add friends list item component

---
+++ resources/js/Mantastudio/FriendsListItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex grayBackground py-2 items-cente m-auto border-b-2 border-t-2 border-black">
+        <img class="flex-none w-12 h-12 ml-4 bg-gray-800 rounded-full" :src="avatar" alt="avatar"/>
+        <div class="flex-grow h-12 text-center text-xl font-medium flex flex-col justify-center items-start">
+            <div class="ml-4">{{username}}</div>
+        </div>
+        <img class="flex-none w-12 h-12 mr-4" src="/images/form/loupe.svg"/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        avatar: String,
+        username: String
+    },
+    setup(props) {  
+        return {
+            avatar: props.avatar,
+            username: props.username
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+    .grayBackground {
+        background-color: #F0EFF0;
+    }
+
+    .grayText {
+        color: #6F6F6F;
+    }
+</style>
