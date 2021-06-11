455d8a320d4cab5b00ed69a4a00bb773cb11c2b4
Nathan Zweifel <nathan.zweifel@heig-vd.ch>
2021-06-11 15:35:12 +0200

    feat: add question frame component

---
+++ resources/js/Mantastudio/QuestionFrame.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="grayBackground h-44">
+        <div class="pt-2 text-center text-sm font-bold">
+            {{currentNumber}}/{{totalNumber}}
+        </div>
+        <div class="mt-6 text-center text-2xl mx-4 flex flex-col justify-center items-start">
+            {{question}}
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentNumber: String,
+        totalNumber: String,
+        question: String
+    },
+    setup(props) {  
+        return {
+            currentNumber: props.currentNumber,
+            totalNumber: props.totalNumber,
+            question: props.question
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+    .grayBackground {
+        background-color: #F0EFF0;
+    }
+
+    .grayText {
+        color: #6F6F6F;
+    }
+</style>
