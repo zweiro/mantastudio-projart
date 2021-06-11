55de18c7d74d077f785159d5a4396f743582bc1e
Nathan Zweifel <nathan.zweifel@heig-vd.ch>
2021-06-11 16:23:03 +0200

    merge: branch 'main' of https://github.com/zweiro/mantastudio-projart into main

---
+++ database/migrations/2021_06_03_131448_create_question_answers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question_answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->boolean('correct');
+            $table->foreignId('question_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('question_answers', function (Blueprint $table) {
+            $table->dropForeign(['question_id']);
+        });
+        Schema::dropIfExists('question_answers');
+    }
+}
---
+++ resources/js/Mantastudio/BattleListItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex grayBackground py-2 items-cente m-auto border-b-2 border-t-2 border-black">
+        <img class="flex-none w-12 h-12 ml-4 bg-gray-800 rounded-full" :src="avatar" alt="avatar"/>
+        <div class="flex-grow h-12 text-center text-3xl font-semibold flex flex-col justify-center items-start ml-4">{{username}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        avatar: String,
+        username: String
+    },
+    setup(props) {  
+        return {
+            avatar: props.avatar,
+            username: props.username
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+    .grayBackground {
+        background-color: #F0EFF0;
+    }
+
+    .grayText {
+        color: #6F6F6F;
+    }
+</style>
