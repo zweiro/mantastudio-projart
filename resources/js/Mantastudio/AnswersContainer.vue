49426a5281917015b0f5bf68e18129a7f4294074
Nathan Zweifel <nathan.zweifel@heig-vd.ch>
2021-06-12 13:14:00 +0200

    feat: add answers items and container component

---
+++ resources/js/Mantastudio/AnswersContainer.vue
@@ -0,0 +1,48 @@
+<template>
+        <div class="flex flex-wrap place-content-center">
+            <!-- <button class="flex-1 h-36 border-4 answer mt-6 text-center text-2xl mx-4 flex flex-col justify-center items-start" :class="{ isRight: rightAnswer }">
+                <div class="mx-auto">{{answer1}}</div>
+            </button>
+            <button class="flex-1 h-36 border-4 answer mt-6 text-center text-2xl mx-4 flex flex-col justify-center items-start" :class="{ isRight: rightAnswer }">
+                <div class="mx-auto">{{answer2}}</div>
+            </button>
+            <button class="flex-1 h-36 border-4 answer mt-6 text-center text-2xl mx-4 flex flex-col justify-center items-start" :class="{ isRight: rightAnswer }">
+                <div class="mx-auto">{{answer3}}</div>
+            </button>
+            <button class="flex-1 h-36 border-4 answer mt-6 text-center text-2xl mx-4 flex flex-col justify-center items-start" :class="{ isRight: rightAnswer }">
+                <div class="mx-auto">{{answer4}}</div>
+            </button> -->
+            <slot></slot>
+        </div>
+</template>
+
+<script>
+export default {
+    props: {
+    },
+    setup(props) {  
+        return {
+
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+    .answer {
+        border-color: #F396A1;
+        background-color: #F0EFF0;
+    }
+
+    .isRight {
+        border-color: #669966;
+        background-color: #669966;
+    }
+
+    .grayText {
+        color: #6F6F6F;
+    }
+
+    button:focus { outline:0 !important; }
+</style>
---
+++ resources/js/Mantastudio/AnswersItem.vue
@@ -0,0 +1,51 @@
+<template>
+    <button class="flex-1 h-36 border-4 answer mt-6 text-center text-2xl mx-4 flex flex-col justify-center items-start" :class="{ isRight: rightAnswer, isWrong: wrongAnswer }">
+        <div class="mx-auto">
+            <slot></slot>
+        </div>
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        rightAnswer: {
+        type: Boolean,
+        default: false
+        },
+        wrongAnswer: {
+        type: Boolean,
+        default: false
+        },
+    },
+    setup(props) {  
+        return {
+            rightAnswer: props.rightAnswer,
+            wrongAnswer: props.wrongAnswer
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+    .answer {
+        border-color: #F396A1;
+        background-color: #F0EFF0;
+    }
+
+    .isRight {
+        border-color: #669966;
+        background-color: #669966;
+    }
+    .isWrong {
+        border-color: #F35D51;
+        background-color: #F35D51;
+    }
+
+    .grayText {
+        color: #6F6F6F;
+    }
+
+    button:focus { outline:0 !important; }
+</style>
