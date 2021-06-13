d5e4bdd1d7b47d97bf127ac19626f066a3572a55
Nathan Zweifel <nathan.zweifel@heig-vd.ch>
2021-06-13 15:18:00 +0200

    feat: add category item and container components

---
+++ resources/js/Mantastudio/CategoryContainer.vue
@@ -0,0 +1,21 @@
+<template>
+        <div class="flex flex-nowrap overflow-x-auto container mt-14">
+            <slot></slot>
+        </div>
+</template>
+
+
+
+<script>
+</script>
+
+<style>
+    .container{
+        -webkit-overflow-scrolling: touch;
+        -ms-overflow-style: -ms-autohiding-scrollbar;
+    }
+
+    .container::-webkit-scrollbar {
+        display: none;
+    }
+</style>
---
+++ resources/js/Mantastudio/CategoryItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <a class="card relative mr-4 w-48">
+            <div class="absolute top-0 left-0 pt-2 ml-2 text-center text-xl font-bold">
+                {{categoryName}}
+            </div>
+        <img class="" :src="src" :alt="alt">
+    </a>
+</template>
+
+<script>
+export default {
+    props: {
+        src: String,
+        alt: String,
+        categoryName: String,
+    },
+    setup(props) {  
+        return {
+            src: props.src,
+            alt: props.alt,
+            categoryName: props.categoryName,
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+    .manta-pink{
+        background-color: #F396A1;
+    }
+
+    .manta-lightgray{
+        background-color: #F0EFF0;
+    }
+
+    .grayText {
+        color: #6F6F6F;
+    }
+
+    .card {
+        flex: 0 0 auto;
+    }
+</style>
